2d660aa1a216f0371021aaae0d0da4d9fe98e5b6
Rohil Saraf <rohilsaraf97@gmail.com>
2022-08-27 12:28:27 +0530

    feat: configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { TransferComponent } from './screens/transfer/transfer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'transfer', component: TransferComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [HomeComponent, TransferComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarItemComponent } from './MyComponents/navbar-item/navbar-item.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { MaterialModule } from './material/material.module';
+import { HeroComponent } from './MyComponents/hero/hero.component';
+import { TransactionCardComponent } from './MyComponents/transaction-card/transaction-card.component';
+import { TransactionsTableComponent } from './MyComponents/transactions-table/transactions-table.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { transactionReducer } from './state/transaction.reducer';
+import { TransactionsFormComponent } from './MyComponents/transactions-form/transactions-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    NavbarItemComponent,
+    NavbarComponent,
+    HeroComponent,
+    TransactionCardComponent,
+    TransactionsTableComponent,
+    FooterComponent,
+    TransactionsFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    StoreModule.forRoot({ balance: transactionReducer }, {}),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
